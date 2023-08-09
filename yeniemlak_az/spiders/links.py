4a973f322fc20c959165eb5232401641a832e816
Səmədov İsmət <ismetsemedov@gmail.com>
2023-08-10 00:01:36 +0400

    check

--- yeniemlak_az/spiders/links.py
+++ yeniemlak_az/spiders/links.py
@@ -7,11 +7,6 @@ class LinksAndPhoneNumbersSpider(scrapy.Spider):
     start_urls = [
         'https://yeniemlak.az/elan/axtar?emlak=1&elan_nov=1&seher%5B%5D=0&metro%5B%5D=0&qiymet=&qiymet2=&mertebe=&mertebe2=&otaq=&otaq2=&sahe_m=&sahe_m2=&sahe_s=&sahe_s2=&page=1'
     ]
-    custom_settings = {
-        'CONCURRENT_REQUESTS': 1,  # 1 second between requests
-
-        'DOWNLOAD_DELAY': 1.0  # Limit concurrent requests to 1
-    }
 
     def parse(self, response):
         # Extract and yield the hrefs on the current page
